fb50e6f9f6e10410f7a47ae801ff1174e215b00c
utmalone <70651540+utmalone@users.noreply.github.com>
2021-05-17 15:02:15 -0400

    test update

--- src/test/java/com/disasterrecovery/EmployeeApp4ApplicationTests.java
+++ src/test/java/com/disasterrecovery/EmployeeApp4ApplicationTests.java
@@ -22,29 +22,29 @@ class EmployeeApp4ApplicationTests {
 //	private MachineService ms;
 	
 
-	@Test
-	void securityAuthentication() {
-//		sc.authenticationProvider();
-//		sc.passwordEncoder();
-	}
 //	@Test
-//	void registrationDtoTest() {
-////		urDto.getClass();
-////		urDto.getEmail();
-////		urDto.getFirstName();
-////		urDto.getLastName();
-////		urDto.getPassword();
-////		urDto.getTitle();
-////		urDto.toString();
+//	void securityAuthentication() {
+////		sc.authenticationProvider();
+////		sc.passwordEncoder();
 //	}
-//	
-//	@Test
-//	void jobsServiceTest() {
-////		js.findAllJobs();
-//	}
-//	
-//	@Test
-//	void machineServiceTest() {
-////		ms.findAllMachines();
-//	}
+////	@Test
+////	void registrationDtoTest() {
+//////		urDto.getClass();
+//////		urDto.getEmail();
+//////		urDto.getFirstName();
+//////		urDto.getLastName();
+//////		urDto.getPassword();
+//////		urDto.getTitle();
+//////		urDto.toString();
+////	}
+////	
+////	@Test
+////	void jobsServiceTest() {
+//////		js.findAllJobs();
+////	}
+////	
+////	@Test
+////	void machineServiceTest() {
+//////		ms.findAllMachines();
+////	}
 }
